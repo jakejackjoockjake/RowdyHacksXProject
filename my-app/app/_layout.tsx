7683ab9e35eb6de4ef20a8a4d82f9fedc3cf7ff0
jakejackjoockjake <jacobglenreeves@gmail.com>
2024-10-26 18:27:48 -0500

    Code

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -3,8 +3,27 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen 
+        name="index" 
+        options={{ 
+          title: 'Home',
+          headerStyle: { backgroundColor: '#d85116' }, // Set the background color of the header
+          headerTintColor: '#ffffff', // Set the color of the title and icons
+          headerTitleStyle: { color: '#ffffff' }, // Set the title text color explicitly
+
+        }} 
+      />
+      <Stack.Screen 
+        name="about" 
+        options={{ 
+          title: 'About',
+          titleColor: "#000000",
+          headerStyle: { backgroundColor: '#d85116' }, // Set the background color of the header
+          headerTintColor: '#ffffff', // Set the color of the title and icons
+          headerTitleStyle: { color: '#ffffff' }, // Set the title text color explicitly
+
+        }} 
+      />
     </Stack>
   );
 }
